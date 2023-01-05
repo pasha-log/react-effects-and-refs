d213a52202ee9f76d3cf66f19d6e58e8bb8240ba
pasha-log <pasha.log@gmail.com>
2023-01-05 12:34:39 -0800

    finished start/stop toggler

--- src/KeepDrawingCard.js
+++ src/KeepDrawingCard.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useRef } from 'react';
 import { v4 as uuid } from 'uuid';
 import axios from 'axios';
 import CreateCard from './Card';
-// import CardDrawTimer from './CardDrawTimer';
 
 const baseURL = 'https://deckofcardsapi.com/api/deck';
 let cardArray = []
@@ -11,6 +10,8 @@ const KeepDrawingCard = () => {
 	const [ deckId, setDeckId ] = useState(null);
 	const [ cards, setCards ] = useState(cardArray);
 	const [ remaining, setRemaining ] = useState();
+	const [ start, setStart ] = useState(false)
+	const firstStart = useRef(true);
     const timerId = useRef();
 
 	useEffect(() => {
@@ -22,28 +23,39 @@ const KeepDrawingCard = () => {
 		loadCard();
 	}, []);
 
-	async function buttonHandler() {
-		timerId.current = setInterval( async () => {
-			const cardRes = await axios.get(`${baseURL}/${deckId}/draw/`);
-			setRemaining(cardRes.data.remaining);
-			const cardSrc = cardRes.data.cards[0].image;
-			let cardArrayCopy = [ ...cards ];
-			cardArrayCopy.push(cardSrc);
-			setCards(cardArrayCopy);
-		}, 1000)
-	}
-	
-    // const stopTimer = () => {
-    //     clearInterval(timerId.current)
-    // }
+	useEffect(() => {
+		if (firstStart.current) {
+		  console.log("first render, don't run useEffect for timer");
+		  firstStart.current = !firstStart.current;
+		  return;
+		}
 
+		if (start) {
+			timerId.current = setInterval( async () => {
+				const cardRes = await axios.get(`${baseURL}/${deckId}/draw/`);
+				setRemaining(cardRes.data.remaining);
+				const cardSrc = cardRes.data.cards[0].image;
+				let cardArrayCopy = [ ...cards ];
+				cardArrayCopy.push(cardSrc);
+				setCards(cardArrayCopy);
+			}, 1000)
+		} else {
+			clearInterval(timerId.current)
+		}
+
+		return () => clearInterval(timerId.current);
+	}, [start]);
+	
+	const toggleStart = () => {
+		setStart(!start)
+	}
 
 	return (
 		<div>
 			{remaining === 0 ? (
 				alert('Error: no cards remaining!')
 			) : (
-				<button onClick={() => buttonHandler()}>Draw a Card Every Second</button>
+				<button onClick={() => toggleStart()}>{!start ? "Start Drawing" : "Stop Drawing"}</button>
 			)}
 			<div className="card-area">{cards.map((card) => <CreateCard cardSrc={card} id={uuid()} />)}</div>
 		</div>
