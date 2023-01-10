0ac7308d78e15796aea6f8c1cc4f4d35f750924b
pasha-log <pasha.log@gmail.com>
2023-01-10 15:12:50 -0800

    Updates

---
+++ precache-manifest.b602adae6ef9bcaff29d5a6a64433be9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f87116c29b11bd6613f2",
+    "url": "/react-effects-and-refs/static/css/main.d79ed6c1.chunk.css"
+  },
+  {
+    "revision": "f87116c29b11bd6613f2",
+    "url": "/react-effects-and-refs/static/js/main.f87116c2.chunk.js"
+  },
+  {
+    "revision": "237fa01f28bcd1fb9833",
+    "url": "/react-effects-and-refs/static/js/1.237fa01f.chunk.js"
+  },
+  {
+    "revision": "26117a0140652069fead",
+    "url": "/react-effects-and-refs/static/js/runtime~main.26117a01.js"
+  },
+  {
+    "revision": "a1586fff7d81a131abc84b6a0d5d5e6a",
+    "url": "/react-effects-and-refs/index.html"
+  }
+];
